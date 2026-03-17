ee2278ce83c1a39b106d11696c13f58ccfb78099
sergioperez-dev <sp7134651@gmail.com>
2026-03-17 18:23:53 -0400

    ll

---
+++ Sistema_Para_Clinica_Dental/CitasForm/AgregarCitas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Para_Clinica_Dental.CitasForm
+{
+    public partial class Agregar : Form
+    {
+        public Agregar()
+        {
+            InitializeComponent();
+        }
+    }
+}
